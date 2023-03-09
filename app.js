b4babc3c38f73c19a2f00317e252072d9f389f33
Mohamed Hannaoui <hannaoui20.mohamed@gmail.com>
2023-03-09 10:32:52 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let audioMap = {
+    'a': new Audio('/sounds/clap.wav'),
+    's': new Audio('/sounds/hihat.wav'),
+    'd': new Audio('/sounds/kick.wav'),
+    'f': new Audio('/sounds/openhat.wav'),
+    'g': new Audio('/sounds/boom.wav'),
+    'h': new Audio('/sounds/ride.wav'),
+    'j': new Audio('/sounds/snare.wav'),
+    'k': new Audio('/sounds/tom.wav'),
+    'l': new Audio('/sounds/tink.wav')
+};
+  
+document.addEventListener('keydown', (e)=> {
+let letter = e.key.toLowerCase();
+if (audioMap.hasOwnProperty(letter)){
+      audioMap[letter].play();
+    }
+  });
+  
