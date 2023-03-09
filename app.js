eef8e399fd1c4e2c83593a27d7cdce62a1ea4abe
Mohamed Hannaoui <hannaoui20.mohamed@gmail.com>
2023-03-09 11:25:18 +0100

    source

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 let audioMap = {
-    'a': new Audio('/sounds/clap.wav'),
-    's': new Audio('/sounds/hihat.wav'),
-    'd': new Audio('/sounds/kick.wav'),
-    'f': new Audio('/sounds/openhat.wav'),
-    'g': new Audio('/sounds/boom.wav'),
-    'h': new Audio('/sounds/ride.wav'),
-    'j': new Audio('/sounds/snare.wav'),
-    'k': new Audio('/sounds/tom.wav'),
-    'l': new Audio('/sounds/tink.wav')
+    'a': new Audio('sounds/clap.wav'),
+    's': new Audio('sounds/hihat.wav'),
+    'd': new Audio('sounds/kick.wav'),
+    'f': new Audio('sounds/openhat.wav'),
+    'g': new Audio('sounds/boom.wav'),
+    'h': new Audio('sounds/ride.wav'),
+    'j': new Audio('sounds/snare.wav'),
+    'k': new Audio('sounds/tom.wav'),
+    'l': new Audio('sounds/tink.wav')
 };
   
 document.addEventListener('keydown', (e)=> {
@@ -16,4 +16,3 @@ if (audioMap.hasOwnProperty(letter)){
       audioMap[letter].play();
     }
   });
-  
